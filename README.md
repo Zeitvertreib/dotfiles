2dee46d12127081e8a0b0b5a1cd5c0b27ed8b71e
mime1 <me@220.z>
2016-03-27 13:19:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# little conky
+
+* does show hours in it,
+* matches w my dotfiles
+* initial, not very advanced (but a bit)
+* dynamic diractory allocation (hopefully)
---
+++ loadup/controller.py
@@ -0,0 +1,25 @@
+import datetime as d
+import stunden
+
+now = d.datetime.now().strftime("%H:%M")
+ds_dict = stunden.doppelstunden
+
+def find_current_DS(my_dict, now):
+    timestate = "pause"
+    for key in my_dict.keys():
+        if my_dict[key][0] <= now:
+            #  timestate = "pause"+str(key)
+            #  print("works for key:", key)
+            #  print(my_dict[key][1])
+            if now <= my_dict[key][1]:
+                #  print("should hook here")
+                timestate = str(key)+".DS"
+                return timestate
+    return timestate
+
+
+
+if __name__ == "__main__":
+    #  print(now)
+    #  print(find_current_DS( ds_dict, now ))
+    print(find_current_DS( ds_dict, now ))
